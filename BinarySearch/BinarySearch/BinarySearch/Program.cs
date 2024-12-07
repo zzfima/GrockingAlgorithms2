64e351c83cd67ed5df79f89c06ba2f9f1a4cf368
zzfima <zzfima@gmail.com>
2024-12-07 18:42:47 +0200

    done

---
+++ BinarySearch/BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,41 @@
+﻿
+namespace BinarySearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int[] ints = new int[100];
+
+            for (int i = 0; i < ints.Length; i++)
+                ints[i] = i * 2;
+
+            for (int i = 0; i < 210; i++)
+                Console.WriteLine($"num {i} exists: {IsNumberExists(ints, i)}");
+
+            Console.ReadLine();
+        }
+
+        private static bool IsNumberExists(int[] ints, int v)
+        {
+            if (ints.Length == 0)
+                return false;
+
+            int middleIndex = ints.Length / 2;
+            int numInTheMiddle = ints[middleIndex];
+
+            if (numInTheMiddle == v)
+                return true;
+
+            if (ints.Length == 1)
+                return false;
+
+            if (numInTheMiddle > v)
+                return IsNumberExists(ints[..middleIndex], v);
+
+            return IsNumberExists(ints[(middleIndex + 1)..], v);
+        }
+    }
+}
