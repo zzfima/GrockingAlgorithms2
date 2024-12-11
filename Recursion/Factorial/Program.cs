4e24248f20f842e7e007b7eb561657c4adc21c49
Efim Zabarsky <[Efim.Zabarsky@kla-tencor.com]>
2024-12-11 19:59:41 +0200

    factorial impl

---
+++ Recursion/Factorial/Program.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Factorial
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var v = Factorial(5);
+			Console.WriteLine(v);
+			Console.ReadLine();
+		}
+
+		private static int Factorial(int v)
+		{
+			if (v == 1)
+			{
+				Console.WriteLine("1");
+				return 1;
+			}
+
+			Console.Write($"{v} * ");
+
+			return v * Factorial(v - 1);
+		}
+	}
+}
