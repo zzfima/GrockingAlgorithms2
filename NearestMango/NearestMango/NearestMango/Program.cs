781bb533c3ebfeab4952df62393aada20a4eef0c
Efim Zabarsky <[Efim.Zabarsky@kla-tencor.com]>
2024-12-16 08:23:26 +0200

    peach seller finder

---
+++ NearestMango/NearestMango/NearestMango/Program.cs
@@ -0,0 +1,48 @@
+﻿
+namespace NearestMango
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Dictionary<string, string[]> sellers = new();
+			sellers["me"] = ["Alice", "Bob", "Clare"];
+
+			sellers["Alice"] = ["Peggy"];
+			sellers["Bob"] = ["Peggy", "Anuj"];
+			sellers["Clare"] = ["Johnny", "Tom"];
+
+			sellers["Peggy"] = [];
+			sellers["Anuj"] = [];
+			sellers["Johnny"] = [];
+			sellers["Tom"] = [];
+
+			Queue<string> queue = new();
+			EnqueueStrings(sellers["me"], queue);
+
+			while (queue.Count > 0)
+			{
+				string seller = queue.Dequeue();
+				if (IsMangoSeller(seller))
+				{
+					Console.WriteLine($"Peach seller {seller}");
+					break;
+				}
+
+				EnqueueStrings(sellers[seller], queue);
+			}
+
+			Console.ReadLine();
+		}
+
+		private static bool IsMangoSeller(string seller) => seller[^1] == 'm';
+
+		static void EnqueueStrings(string[] strings, Queue<string> queue)
+		{
+			foreach (string s in strings)
+			{
+				queue.Enqueue(s);
+			}
+		}
+	}
+}
---
+++ NearestMango/NearestMango/NearestMango2/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace NearestMango2
+{
+	public enum FruitType
+	{
+		Mango, Peach, Apple
+	}
+	public class FruitsSeller(string name, FruitType fruit)
+	{
+		public string Name { get; } = name;
+		public FruitType Fruit { get; } = fruit;
+	}
+
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			FruitsSeller me = new("me", FruitType.Mango);
+			FruitsSeller alice = new("Alice", FruitType.Mango);
+			FruitsSeller bob = new("Bob", FruitType.Mango);
+			FruitsSeller clare = new("Clare", FruitType.Apple);
+			FruitsSeller peggy = new("Peggy", FruitType.Mango);
+			FruitsSeller anunj = new("Anuj", FruitType.Mango);
+			FruitsSeller johny = new("Johnny", FruitType.Mango);
+			FruitsSeller tom = new("Tom", FruitType.Peach);
+
+			Dictionary<FruitsSeller, FruitsSeller[]> sellers = new();
+			sellers[me] = [alice, bob, clare];
+
+			sellers[alice] = [peggy];
+			sellers[bob] = [peggy, anunj];
+			sellers[clare] = [johny, tom];
+
+			sellers[peggy] = [];
+			sellers[anunj] = [];
+			sellers[johny] = [];
+			sellers[tom] = [];
+
+			Queue<FruitsSeller> queue = new();
+			EnqueueStrings(sellers[me], queue);
+
+			while (queue.Count > 0)
+			{
+				FruitsSeller seller = queue.Dequeue();
+				if (seller.Fruit == FruitType.Peach)
+				{
+					Console.WriteLine($"Peach seller {seller.Name}");
+					break;
+				}
+
+				EnqueueStrings(sellers[seller], queue);
+			}
+
+			Console.ReadLine();
+		}
+		static void EnqueueStrings(FruitsSeller[] sellers, Queue<FruitsSeller> queue)
+		{
+			foreach (FruitsSeller s in sellers)
+			{
+				queue.Enqueue(s);
+			}
+		}
+	}
+}
