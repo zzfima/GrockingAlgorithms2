7dc8ad87e60b646a41c0eade818e63d6b383d1c0
zzfima <zzfima@gmail.com>
2024-12-21 08:40:38 +0200

    Add IsPerfectSquare

--- BinarySearch/BinarySearch/BinarySearch/Program.cs
+++ BinarySearch/BinarySearch/BinarySearch/Program.cs
@@ -7,15 +7,37 @@ namespace BinarySearch
         {
             Console.WriteLine("Hello, World!");
 
+            var b1 = IsPerfectSquare(2147483647);
+            var b2 = IsPerfectSquare(2147302921);
+
             int[] ints = new int[100];
 
             for (int i = 0; i < ints.Length; i++)
                 ints[i] = i * 2;
 
             for (int i = 0; i < 210; i++)
-                Console.WriteLine($"num {i} exists: {IsNumberExistsRecursion(ints, i)}");
+            {
+                Console.WriteLine($"Rec: num {i} exists: {IsNumberExistsRecursion(ints, i)}");
+                Console.WriteLine($"Loop: num {i} index: {IsNumberExists(ints, i)}");
+            }
 
             Console.ReadLine();
+        }
+
+        public static bool IsPerfectSquare(int num)
+        {
+            for (Int64 i = 0, j = num; i <= j;)
+            {
+                Int64 middle = (j + i) / 2;
+                if (middle * middle == num)
+                    return true;
+                if (middle * middle > num)
+                    j = middle - 1;
+                else
+                    i = middle + 1;
+            }
+
+            return false;
         }
 
         private static bool IsNumberExistsRecursion(int[] ints, int v)
@@ -36,6 +58,26 @@ namespace BinarySearch
                 return IsNumberExistsRecursion(ints[..middleIndex], v);
 
             return IsNumberExistsRecursion(ints[(middleIndex + 1)..], v);
+        }
+
+        private static int IsNumberExists(int[] ints, int v)
+        {
+            int highIndex = ints.Length - 1;
+            int lowIndex = 0;
+
+            while (lowIndex < highIndex)
+            {
+                int middleIndex = (highIndex - lowIndex) / 2;
+
+                if (ints[middleIndex] == v)
+                    return middleIndex;
+                else if (ints[middleIndex] > v)
+                    highIndex = middleIndex;
+                else
+                    lowIndex = middleIndex;
+            }
+
+            return -1;
         }
     }
 }
